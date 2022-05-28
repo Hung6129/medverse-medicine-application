234a031f7b6b2c4b733c390d6efcae5a6e192ee2
PhamQuocDuy <duy.187pm13901@vanlanguni.vn>
2022-05-28 16:12:37 +0700

    DRUGINFO-288-flutter-ui-disclaimer - Create new file login unit test

---
+++ test/authentication/login_page_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:medverse_mobile_app/services/auth_service.dart';
+import 'package:mockito/mockito.dart';
+
+class MockAuth extends Mock implements AuthService{}
+
+void main() {
+  //testWidgets('email or password is ', callback)
+}
