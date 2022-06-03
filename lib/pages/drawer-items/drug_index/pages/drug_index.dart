6efe65a869dd4ba55ea0af1ebdfb5f81c2a39dc6
PhamQuocDuy <duy.187pm13901@vanlanguni.vn>
2022-06-03 21:39:15 +0700

    DRUGINFO-288-flutter-ui-disclaimer - Add text search for drug index

--- lib/pages/drawer-items/drug_index/pages/drug_index.dart
+++ lib/pages/drawer-items/drug_index/pages/drug_index.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:medverse_mobile_app/models/drug_model.dart';
+import 'package:medverse_mobile_app/widgets/icon_badge.dart';
+import '../../../../utils/data_dao.dart';
 import '../widgets/page_index_drug.dart';
 import '/theme/palette.dart';
 import '/utils/app_text_theme.dart';
@@ -45,6 +48,13 @@ class _MedicineDictionaryState extends State<DrugIndex> {
     'Y',
     'Z'
   ];
+
+  /// Method search keyword in SQLite
+  Future<List<DrugModel>> search(String search) async {
+    var list = await DataDao().searchDrugs(search);
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
     /// Tab bar
@@ -89,32 +99,149 @@ class _MedicineDictionaryState extends State<DrugIndex> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Palette.mainBlueTheme,
-        title: Text(
-          'Danh sách chỉ mục thuốc',
-          style: MobileTextTheme().appBarStyle,
-        ),
+        title: isSearching
+            ? TextField(
+                decoration: InputDecoration(
+                  hintText: "Mời bạn nhập từ khóa",
+                  hintStyle: MobileTextTheme().introContentFont,
+                ),
+                onChanged: (searchResult) {
+                  setState(() {
+                    searchWord = searchResult;
+                  });
+                },
+                style: MobileTextTheme().kBodyTextStyle,
+              )
+            : Text(
+                'Danh sách chỉ mục thuốc',
+                style: MobileTextTheme().appBarStyle,
+              ),
         centerTitle: true,
+        actions: [
+          isSearching
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isSearching = false;
+                    });
+                  },
+                  icon: Icon(Icons.cancel))
+              : IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isSearching = true;
+                    });
+                  },
+                  icon: Icon(Icons.search),
+                ),
+        ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            SizedBox(height: 1.0),
-            DefaultTabController(
-              length: 26, // length of tabs
-              initialIndex: 0,
+      body: isSearching
+          ? FutureBuilder<List<DrugModel>>(
+              /// Check status if user input available keyword then show the result. If not, show all data
+              future: search(searchWord),
+              builder: (context, snapshot) {
+                /// Check if already have data
+                if (snapshot.hasData) {
+                  var list = snapshot.data;
+
+                  /// Show data
+                  return ListView.builder(
+                    itemCount: list.length,
+                    itemBuilder: (context, i) {
+                      var word = list[i];
+                      return GestureDetector(
+                        onTap: () {
+                          /*Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage(list[i])));*/
+                        },
+                        child: SizedBox(
+                          height: 100,
+                          child: Card(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Flexible(
+                                  child: Text(
+                                    word.name,
+                                    style: MobileTextTheme()
+                                        .healthProfileNoDataTextStyle,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 50,
+                                ),
+                                Flexible(
+                                  child: Text(
+                                    word.labeller,
+                                    textAlign: TextAlign.start,
+                                    style: MobileTextTheme().tabBarStyle,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+
+                  /// Show message if it have no data
+                } else {
+                  return Center(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage(
+                                  "assets/icons/NoData.png",
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          child: Text(
+                            "Không tìm thấy dữ liệu",
+                            style: TextStyle(
+                              color: Palette.red,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 100),
+                      ],
+                    ),
+                  );
+                }
+              },
+            )
+          : SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  __tabBar(),
-                  SizedBox(height: 20),
-                  __tabBarView(),
+                  SizedBox(height: 1.0),
+                  DefaultTabController(
+                    length: 26, // length of tabs
+                    initialIndex: 0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        __tabBar(),
+                        SizedBox(height: 20),
+                        __tabBarView(),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
